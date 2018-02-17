b66f38e65f573192043e483cd7398f78f9a9ef62
Mark Stier <stier020@Marks-MacBook-Pro-2.local>
2018-02-17 11:01:40 -0600

    Added filter and reduce

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
 var express = require("express");
-var {MapHandler, ConcatMapHandler, FlatHandler} = require("megaeval").Server;
+var {MapHandler, ConcatMapHandler, FlatMapHandler, ReduceHandler, FilterHandler, FlatFilterHandler, ConcatFilterHandler} = require("megaeval").Server;
 
 var app = express();
 var port = 8070;
@@ -9,7 +9,11 @@ if(process.argv[2]){
 }
 
 app.post("/map", MapHandler);
-app.post("/flat-map", FlatHandler);
+app.post("/flat-map", FlatMapHandler);
 app.post("/concat-map", ConcatMapHandler);
+app.post("/filter", FilterHandler);
+app.post("/flat-filter", FlatFilterHandler);
+app.post("/concat-filter", ConcatFilterHandler);
+app.post("/reduce", ReduceHandler);
 
 app.listen(port);
