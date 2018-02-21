2d2e17293ec40632ba7436ae289f82e18e0e2257
Mark Stier <stier020@Marks-MacBook-Pro-2.local>
2018-02-21 08:47:50 -0600

    Added auth

--- server.js
+++ server.js
@@ -4,10 +4,27 @@ var {MapHandler, ConcatMapHandler, FlatMapHandler, ReduceHandler, FilterHandler,
 
 var app = express();
 var port = 8070;
-if(process.argv[2]){
-  port = process.argv[2];
+var auth = undefined;
+
+for(var i in process.argv){
+  if(process.argv[i].indexOf("--port=") >= 0){
+    port = process.argv[i].split("=")[1];
+  }
+  if(process.argv[i].indexOf("--auth=") >= 0){
+    auth = process.argv[i].split("=")[1];
+  }
 }
 
+
+function authHandler(req, res, next){
+  var authHeader = req.headers["Auth"];
+  if(authHeader === auth){
+    next();
+    return;
+  }
+  next(new Error("Unauthorized"));
+}
+app.use(authHandler)
 app.post("/map", MapHandler);
 app.post("/flat-map", FlatMapHandler);
 app.post("/concat-map", ConcatMapHandler);
